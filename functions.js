e3342a9dafcd2e522cae139171e20911b8d7ac5b
Maddy <184935886+Maddy-bee@users.noreply.github.com>
2024-12-29 16:18:59 +0000

    Fix #5 create functions.js and starting functions

---
+++ functions.js
@@ -0,0 +1,31 @@
+import { promises as fs } from "node:fs";
+
+import characters from "./characters.json" with {type: "json" };
+
+// function to read all the characters from the JSON file. Try and catch statements handle errors 
+async function readCharacters() {
+    try {
+      const characterData = await fs.readFile(characters, "utf8");
+      return JSON.parse(characterData);
+    } catch (error) {
+      console.error("Error reading file:", error);
+      return null;
+    }
+  }
+
+  // function to create a new character and turn it into JSON format. Try and catch statements handle errors 
+  async function writeCharacter(characterData) {
+    try {
+      await fs.writeFile(characters, JSON.stringify(characterData), "utf8");
+      return true;
+    } catch (error) {
+      console.error("Error writing file:", error);
+      return false;
+    }
+  }
+
+  // function to get all characters 
+  export async function getAllCharacters() {
+    const characters = await readCharacters();
+    return characters;
+  }
